a5b39440e83b129d05ab15634f8ccbf95db2ddad
Your Name <liemhoanglong@gmail.com>
2019-12-06 17:24:42 +0700

    up front-end

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-var express =require('express');
-var exphbs = require('express-handlebars');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.engine('hbs', exphbs(
-    {
-        defaultLayout: 'main.hbs',
-        layoutsDir: 'views/_layout'
-    })
-);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(express.static('public'));
 
-//-- Trang đăng nhập
-app.get('/',(req,res)=>{
-    res.end('login');
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//middle ware
-app.use('/admin',require('./routes/admin/index.js'));
+app.use('/', indexRouter);
+app.use('/', usersRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-//listen port 3000
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
